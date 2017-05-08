a24ace269c59f341173780a434962ede7c1e4144
Amber Murray <amber.applebee@gmail.com>
2017-05-08 12:07:12 -0700

    basic layout view working, added partials folder, bootstrap stuff, resources folders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
+//HOME PAGE
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Galvanize Reads' })
+})
 
-module.exports = router;
+module.exports = router
